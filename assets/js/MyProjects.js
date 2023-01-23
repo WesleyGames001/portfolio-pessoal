179c57e08e70c4623337caced92352e47c2bebfe
Wesley G. A. Ramos <dev.wesleygabriel@gmail.com>
2023-01-22 21:02:35 -0300

    22/01/2023

---
+++ assets/js/MyProjects.js
@@ -0,0 +1,25 @@
+const data = {
+    invyctusmc: {
+        title: "InvyctusMC",
+        description: "",
+        video: "https://youtu.be/kj7nhb6F",
+        logo: "assets/images/invyctus-300x300.png"
+    },
+
+    smeltingmc: {
+        title: "SmeltingMC",
+        description: "",
+        video: "https://youtu.be/kj7nhb6F",
+        logotipo: "assets/images/smelting-300x300.png"
+    }
+}
+
+const parent = document.getElementById("projects-show-more")
+
+function updateProject(id) {
+    // parent.style.display = "flex"
+    
+    console.log("a tela foi atualizada");
+}
+
+updateProject("invyctus");
